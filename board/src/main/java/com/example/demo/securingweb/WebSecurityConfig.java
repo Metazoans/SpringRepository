cc7e1048adef7a262c05b51d333382daa0d6da35
Metazoans <john3620@naver.com>
2025-01-20 17:50:27 +0900

    2025-01-20 수업 과제있음

--- board/src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
+++ board/src/main/java/com/example/demo/securingweb/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.securingweb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig {
 	
 	// 비밀번호 암호화
